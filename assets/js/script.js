d5b54a7e779d3f239cf4700ec8a3c738548bec20
AbdulConsole <hzwedt@gmail.com>
2024-07-23 12:34:26 +0100

    update general index page

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+fetch('project.json')
+  .then(response => response.json())
+  .then(data => {
+    const projectContainer = document.getElementById('project-container');
+    data.projects.forEach(project => {
+      const projectElement = document.createElement('div');
+      projectElement.classList.add('project');
+      projectElement.innerHTML = `
+                <img src="${project.thumbnail}" alt="${project.name}">
+                <div class="project-name">${project.name}</div>
+            `;
+      projectElement.addEventListener('click', () => {
+        window.location.href = project.url;
+      });
+      projectContainer.appendChild(projectElement);
+    });
+  })
+  .catch(error => console.error('Error loading projects:', error));
